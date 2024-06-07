777f8170109fff2ddeb423e5f6b1dacc0c8cf3f0
bruno <blombardo@eagerworks.com>
2024-06-07 14:32:58 -0300

    handle errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :last_name, :company_name, presence: true
 end
