ce8eba6a672998e858b8d05634e89bcf901d930f
bruno <blombardo@eagerworks.com>
2024-06-10 15:14:36 -0300

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  add_flash_types :info, :error, :warning
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
