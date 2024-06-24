8a881d81add30c10befe0c525e46b9405e31eae1
bruno <blombardo@eagerworks.com>
2024-06-24 16:03:01 -0300

    index view

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,4 +1,11 @@
 class GiftsController < ApplicationController
   def index
+    @gifts = Gift.all
+  end
+
+  private
+
+  def gift_params
+    params.require(:gift).permit(:name, :valoration, :price, :supplier_id, :image)
   end
 end
